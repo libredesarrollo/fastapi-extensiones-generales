ce7f1ac7168845560f7197100b0ef41da0386363
Andres Cruz <desarrollolibre.net@gmail.com>
2026-02-25 10:11:25 -0400

    cache

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import redis.asyncio as redis
 # import fastapi_limiter
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
 # Importamos tus routers
 from myemail import email_router
@@ -9,6 +11,7 @@ from myuser import auth_router
 from myenv import env_router
 from mylimiter import limiter_router
 from mylogging import logging_router
+from mycache import cache_router
 
 
 from database.database import Base, engine, get_database_session
@@ -24,6 +27,9 @@ async def lifespan(app: FastAPI):
         encoding="utf-8", 
         decode_responses=True
     )
+    
+    # Inicializar FastAPI Cache con el backend de Redis
+    FastAPICache.init(RedisBackend(redis_instance), prefix="fastapi-cache")
     
     # # Inicializar el limitador (usando la referencia al módulo si la clase falla)
     # await fastapi_limiter.FastAPILimiter.init(redis_instance)
@@ -48,6 +54,7 @@ app.include_router(auth_router, prefix='/user')
 app.include_router(env_router, prefix='/config')
 app.include_router(limiter_router, prefix='/limiter')
 app.include_router(logging_router, prefix='/logging')
+app.include_router(cache_router, prefix='/cache')
 
 # app.include_router(
 #     MyCRUDRouter(schema=Category, prefix="/categories", tags=["Categories"])
@@ -56,5 +63,8 @@ app.include_router(
     MyCRUDRouter(schema=Task, prefix="/tasks", tags=["Tasks"])
 )
 app.include_router(
-    SQLCRUDRouter(schema=Category, model=CategoryModel,get_db_func=get_database_session, prefix="/categories", tags=["Categories"])
+    SQLCRUDRouter(schema=Category, 
+                  model=CategoryModel,
+                  get_db_func=get_database_session, 
+                  prefix="/categories", tags=["Categories"])
 )
